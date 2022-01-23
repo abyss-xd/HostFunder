fdb27107125dea50236f1408b8a25e0902be3435
Abyss <57658642+abyss-xd@users.noreply.github.com>
2022-01-23 15:40:57 +0100

    Create tempconfig.php

---
+++ tempconfig.php
@@ -0,0 +1,24 @@
+<?php
+
+//Your hosting's name
+$hostname = "Hostname";
+
+//Description for SEO & Embeds
+$descp = "A simple description";
+
+//currency name
+$currname = "Credits";
+
+//url for your logo
+$logo = "/images/logo.png";
+
+//Url for your favicon
+$favurl = "/images/favicon.jpg";
+
+//Your discord invite
+$discordinv = "https://discord.com/invite/";
+
+//your panel url
+$panelurl = "https://panel.example.com";
+
+?>
