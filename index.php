a08895309bbb5f8694dde6099de0dc0c1d818a8b
Abyss <57658642+abyss-xd@users.noreply.github.com>
2022-01-23 11:12:50 +0100

    Update index.php
    Added base structure for landing page

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 
-//This will be the landing page
-
 ?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?php echo $hostname?></title>
+  <meta name="description" content="<?php echo $descp?>">
+  <meta name="author" content="SitePoint">
+
+  <meta property="og:title" content="<?php echo $hostname?>">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?php echo $url?>">
+  <meta property="og:description" content="<?php echo $descp?>">
+  <meta property="og:image" content="<?php echo $thumburl?>">
+
+  <link rel="icon" href="<?php echo $faviconico?>">
+  <link rel="icon" href="<?php echo $faviconsvg?>" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="<?php echo $faviconapple?>">
+
+  <link rel="stylesheet" href="css/landing.css?v=1.0">
+
+</head>
+
+<body>
+
+<h1>Test<h1>
+
+
+  <script src="js/landing.js"></script>
+</body>
+</html>
