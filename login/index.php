c7d702ff67b8f4de93bd2e4cb00f51078fd28acc
Abyss <57658642+abyss-xd@users.noreply.github.com>
2022-01-23 16:00:45 +0100

    Update index.php

--- login/index.php
+++ login/index.php
@@ -1,3 +1,34 @@
+<?php 
+
+require "/tempconfig.php";
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?php echo $hostname?></title>
+  <meta name="description" content="<?php echo $descp?>">
+  <meta name="author" content="SitePoint">
+
+  <meta property="og:title" content="<?php echo $hostname?>">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?php echo $url?>">
+  <meta property="og:description" content="<?php echo $descp?>">
+  <meta property="og:image" content="<?php echo $thumburl?>">
+
+  <link rel="stylesheet" href="login.css">
+
+  <link rel="shortcut icon" type="image/jpg" href="<?php echo $favurl"/>
+
+  <link rel="stylesheet" href="css/landing.css?v=1.0">
+
+</head>
+
 <div class="container">
 	<div class="app front">
 		<div class="header">
