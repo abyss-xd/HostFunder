22ff2af96d79e80d99a0fed5eb351c3b2b2e270b
Abyss <57658642+abyss-xd@users.noreply.github.com>
2022-01-24 09:47:31 +0100

    Update index.php

--- dbauth/index.php
+++ dbauth/index.php
@@ -1,1 +1,16 @@
+<?php 
 
+require "/tempconfig.php";
+
+$servername = $hostname;
+$username = $db_user;
+$password = $db_user_pass;
+$dbname = $dbname;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
