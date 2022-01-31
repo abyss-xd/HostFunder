3bc744e4a530e0ec0dfe3ccd97dc9adcedd57fb7
Abyss <57658642+abyss-xd@users.noreply.github.com>
2022-01-31 08:43:57 +0100

    Update index.php

--- login/index.php
+++ login/index.php
@@ -28,5 +28,104 @@ require "/tempconfig.php";
   <link rel="stylesheet" href="css/landing.css?v=1.0">
 
 </head>
-<body>
-</body>
+   <body>
+       
+                <div class="error">
+             
+             <h1><?php 
+             
+             $error1=$_GET["pgeterror"];
+             if ($error1 == 1) {
+                 
+                 echo "We could not find your user or password in our databases.";
+                 
+             }
+             
+             ?></h1>
+             <h1><?php
+             
+             $error2=$_GET["pinco"];
+             if ($error2 == 1) {
+                 
+                 echo "Your user or password is incorrect.";
+                 
+             }
+             
+             ?></h1>
+         </div>
+       
+       
+      <div class="wrapper">
+         <div class="title-text">
+            <div class="title login">
+               Gems Login
+            </div>
+            <div class="title signup">
+               Gems Signup
+            </div>
+         </div>
+         <div class="form-container">
+            <div class="slide-controls">
+               <input type="radio" name="slide" id="login" checked>
+               <input type="radio" name="slide" id="signup">
+               <label for="login" class="slide login">Login</label>
+               <label for="signup" class="slide signup">Signup</label>
+               <div class="slider-tab"></div>
+            </div>
+            <div class="form-inner">
+               <form action="login.php" class="login" method="post">
+                  <div class="field">
+                     <input type="text" name="user" placeholder="Username" required>
+                  </div>
+                  <div class="field">
+                     <input type="password" name="pass" placeholder="Password" required>
+                  </div>
+                  <div class="pass-link">
+                     <a href="forgot.php">Forgot password?</a>
+                  </div>
+                  <div class="field btn">
+                     <div class="btn-layer"></div>
+                     <input type="submit" value="Login">
+                  </div>
+                  <div class="signup-link">
+                     Not a member? <a href="">Signup now</a>
+                  </div>
+               </form>
+               <form action="signup.php" class="signup" method="post">
+                  <div class="field">
+                     <input type="email" name="email" placeholder="Email Address" required>
+                  </div>
+                  <div class="field">
+                     <input type="text" name="username" placeholder="Username" required>
+                  </div>
+                  <div class="field">
+                     <input type="password" name="pass" placeholder="Password" required>
+                  </div>
+                  <div class="field btn">
+                     <div class="btn-layer"></div>
+                     <input type="submit" value="Signup">
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+      <script>
+         const loginText = document.querySelector(".title-text .login");
+         const loginForm = document.querySelector("form.login");
+         const loginBtn = document.querySelector("label.login");
+         const signupBtn = document.querySelector("label.signup");
+         const signupLink = document.querySelector("form .signup-link a");
+         signupBtn.onclick = (()=>{
+           loginForm.style.marginLeft = "-50%";
+           loginText.style.marginLeft = "-50%";
+         });
+         loginBtn.onclick = (()=>{
+           loginForm.style.marginLeft = "0%";
+           loginText.style.marginLeft = "0%";
+         });
+         signupLink.onclick = (()=>{
+           signupBtn.click();
+           return false;
+         });
+      </script>
+   </body>
